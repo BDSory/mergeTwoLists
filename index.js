ff0f01cae64f21cc49451921c7790dffc5d94388
Bryan S <superbee45@gmail.com>
2022-09-20 14:21:16 -0500

    working and acceptable

--- index.js
+++ index.js
@@ -9,5 +9,24 @@ Every close bracket has a corresponding open bracket of the same type.
  * @return {boolean}
  */
  var isValid = function(s) {
-    
-};
+  const leftSymbols = [];  
+  for(let i =0; i < s.length; i++) {
+      if(s[i] === '(' || s[i] === '{' || s[i] === '[') {
+        leftSymbols.push(s[i]);
+      } else if (s[i] === ')' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '(') {
+        leftSymbols.pop();
+      } else if (s[i] === '}' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '{') {
+        leftSymbols.pop();
+      } else if (s[i] === ']' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '[') {
+        leftSymbols.pop();
+      } else {
+        return false;
+      }
+  }
+  return leftSymbols.length === 0;
+}
+console.log(isValid("()"));
+console.log(isValid("()[]{}"));
+console.log(isValid("(]"));
+console.log(isValid("([)]"));
+console.log(isValid("{[]}"));
