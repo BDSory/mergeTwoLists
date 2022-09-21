f6349c958c467fa5a782c6231a78db654a4b0593
Bryan S <superbee45@gmail.com>
2022-09-21 10:53:00 -0500

    works; Leetcode won't accept; like it better than other submissions

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
-/**Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
-An input string is valid if:
-
-Open brackets must be closed by the same type of brackets.
-Open brackets must be closed in the correct order.
-Every close bracket has a corresponding open bracket of the same type.
- * @param {string} s
- * @return {boolean}
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
  */
- var isValid = function(s) {
-  const leftSymbols = [];  
-  for(let i =0; i < s.length; i++) {
-      if(s[i] === '(' || s[i] === '{' || s[i] === '[') {
-        leftSymbols.push(s[i]);
-      } else if (s[i] === ')' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '(') {
-        leftSymbols.pop();
-      } else if (s[i] === '}' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '{') {
-        leftSymbols.pop();
-      } else if (s[i] === ']' && leftSymbols.length !== 0 && leftSymbols[leftSymbols.length -1] === '[') {
-        leftSymbols.pop();
-      } else {
-        return false;
-      }
-  }
-  return leftSymbols.length === 0;
-}
-console.log(isValid("()"));
-console.log(isValid("()[]{}"));
-console.log(isValid("(]"));
-console.log(isValid("([)]"));
-console.log(isValid("{[]}"));
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
 
-//Runtime: 105 ms, faster than 40.85% of JavaScript online submissions for Valid Parentheses.
-//Memory Usage: 41.7 MB, less than 98.58% of JavaScript online submissions for Valid Parentheses.
+var mergeTwoLists = function(list1, list2) {
+    const mergedList = list1.concat(list2)
+    return mergedList.sort();
+};
